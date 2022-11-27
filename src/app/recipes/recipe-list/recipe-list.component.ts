c0ac40d5171f773ed9e0c3d36c6450445649ef8e
VovaHash <vovahash01@gmail.com>
2022-11-27 22:50:43 +0200

    navigation via event

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,7 +13,7 @@ export class RecipeListComponent implements OnInit {
     new Recipe('Test Recipe 1','This is just a dummy test recipe',
     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341'),
     new Recipe('Test Recipe ee','Another one',
-    'https://assets.epicurious.com/photos/62f16ed5fe4be95d5a460eed/4:6/w_3087,h_4631,c_limit/RoastChicken_RECIPE_080420_37993.jpg')
+    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341')
     
   ];
 
